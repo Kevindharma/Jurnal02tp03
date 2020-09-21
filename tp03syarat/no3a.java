befa9bfce765bd519dc1b727cfefc8b2936ccb3d
AwaNN24 <47474391+AwaNN24@users.noreply.github.com>
2020-09-21 14:49:34 +0700

    no3a.java

---
+++ tp03syarat/no3a.java
@@ -0,0 +1,10 @@
+public class no3a {
+  public static void main(String[]args){
+    int permen = 12;
+    int jumlah_orang = 4;
+    int hasil = permen / jumlah_orang;
+    int sisa = hasil - 3;
+    System.out.println(hasil);
+    System.out.println(sisa);
+  }
+}
