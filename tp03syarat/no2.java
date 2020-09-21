1567eed95645985c8251eb1252d783187de78966
AwaNN24 <47474391+AwaNN24@users.noreply.github.com>
2020-09-21 14:49:05 +0700

    no1.java

---
+++ tp03syarat/no2.java
@@ -0,0 +1,8 @@
+public class no2{
+   public static void main(String[]args){
+      int umur = 20;
+      String name = "Kevin";
+      System.out.println("Halo," + name);
+      System.out.println("Ternyata usiamu baru " + umur + " tahun ya");
+   }
+}
